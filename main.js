5a7128384bf064811835a0d0cb03c4a2b1856337
tmuchicken <tmuchicken@yahoo.co.jp>
2019-01-16 14:35:41 +0900

    overture

--- main.js
+++ main.js
@@ -42,7 +42,8 @@ var playSound = function(buffer) {
 // main
 window.onload = function() {
   // サウンドを読み込む
-  getAudioBuffer('se.mp3', function(buffer) {
+  //getAudioBuffer('se.mp3', function(buffer) {
+    getAudioBuffer('Overture.mp3', function(buffer) {
     // 読み込み完了後にボタンにクリックイベントを登録
     var btn = document.getElementById('btn');
     btn.onclick = function() {
